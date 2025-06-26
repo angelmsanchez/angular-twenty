aa71ae5479d8dd8d3ff79be14a00adaa3482077f
angel muñoz <angelmsanchez85@gmail.com>
2025-06-26 11:05:03 +0200

    add primeng

--- src/app/task/components/task.ts
+++ src/app/task/components/task.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Button } from '../../shared/components/button/button';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.html',
   styleUrl: './task.scss',
-  imports: [],
+  imports: [Button],
 })
 export class Task {
   protected title = 'task-twenty';
